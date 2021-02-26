4dbe6f891225eadf9c8c72c7e9f148fa12ab290e
mialskywalker <kalqga123@gmail.com>
2021-02-26 19:34:09 +0200

    26.02.21

---
+++ defining_classes/exercises/spoopify/song.py
@@ -0,0 +1,25 @@
+from defining_classes.exercises.spoopify.band import Band
+from defining_classes.exercises.spoopify.album import Album
+
+
+class Song:
+    def __init__(self, name: str, length: float, single: bool):
+        self.name = name
+        self.length = length
+        self.single = single
+
+    def get_info(self):
+        return f"{self.name} - {self.length}"
+
+
+song = Song("Running in the 90s", 3.45, False)
+print(song.get_info())
+album = Album("Initial D", song)
+second_song = Song("Around the World", 2.34, False)
+print(album.add_song(second_song))
+print(album.details())
+print(album.publish())
+band = Band("Manuel")
+print(band.add_album(album))
+print(band.remove_album("Initial D"))
+print(band.details())
