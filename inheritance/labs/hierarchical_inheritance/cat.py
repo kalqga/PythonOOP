73ddde8b75c1ffbadd0f007c6303f2b757da7587
mialskywalker <kalqga123@gmail.com>
2021-03-09 14:41:16 +0200

    09.03.21

---
+++ inheritance/labs/hierarchical_inheritance/cat.py
@@ -0,0 +1,8 @@
+from inheritance.labs.hierarchical_inheritance.animal import Animal
+
+
+class Cat(Animal):
+
+    @staticmethod
+    def meow():
+        return "meowing..."
---
+++ inheritance/labs/hierarchical_inheritance/dog.py
@@ -0,0 +1,8 @@
+from inheritance.labs.hierarchical_inheritance.animal import Animal
+
+
+class Dog(Animal):
+
+    @staticmethod
+    def bark():
+        return "barking..."
