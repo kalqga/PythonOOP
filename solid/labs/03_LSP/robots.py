a0814358e58bd2e456cd41aecf78b1da2bbbdefb
mialskywalker <kalqga123@gmail.com>
2021-03-17 10:55:51 +0200

    17.03.21

--- solid/labs/03_LSP/robots.py
+++ solid/labs/03_LSP/robots.py
@@ -1,27 +1,34 @@
-class Robot:
+from abc import abstractmethod, ABC
+
+
+class Robot(ABC):
     def __init__(self, type):
         self.type = type
 
     def get_type(self):
         return self.type
 
+    @abstractmethod
+    def sensors_count(self):
+        pass
+
 
 class Android(Robot):
-    def android_senzors_count(self):
+    def sensors_count(self):
         return 4
 
 
 class Chappie(Robot):
-    def chappie_senzors_count(self):
+    def sensors_count(self):
         return 6
 
 
 def count_robot_senzors(robots: list):
     for robot in robots:
         if isinstance(robot, Android):
-            print(robot.android_senzors_count())
+            print(robot.sensors_count())
         elif isinstance(robot, Chappie):
-            print(robot.chappie_senzors_count())
+            print(robot.sensors_count())
 
 
 robots = [Android('Robocop'), Chappie('XIX')]
