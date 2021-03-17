af5ed1c32d254d31a70d68c9a94dc4422ac564dd
mialskywalker <kalqga123@gmail.com>
2021-03-17 11:11:25 +0200

    17.03.21

--- solid/labs/04_ISP/entertainment_system.py
+++ solid/labs/04_ISP/entertainment_system.py
@@ -1,11 +1,41 @@
-class EntertainmentDevice:
-    def connect_to_device_via_hdmi_cable(self, device): pass
-    def connect_to_device_via_rca_cable(self, device): pass
-    def connect_to_device_via_ethernet_cable(self, device): pass
+from abc import ABC, abstractmethod
+
+
+class Connector(ABC):
+    @abstractmethod
+    def connect(self, device):
+        pass
+
+
+class PowerConnector(Connector):
+    def connect(self, device):
+        self.connect_device_to_power_outlet(device)
+
     def connect_device_to_power_outlet(self, device): pass
 
 
-class Television(EntertainmentDevice):
+class HDMIConnector(Connector):
+    def connect(self, device):
+        self.connect_to_device_via_hdmi_cable(device)
+
+    def connect_to_device_via_hdmi_cable(self, device): pass
+
+
+class RCAConnector(Connector):
+    def connect(self, device):
+        self.connect_to_device_via_rca_cable(device)
+
+    def connect_to_device_via_rca_cable(self, device): pass
+
+
+class EthernetConnector(Connector):
+    def connect(self, device):
+        self.connect_to_device_via_ethernet_cable(device)
+
+    def connect_to_device_via_ethernet_cable(self, device): pass
+
+
+class Television(PowerConnector, RCAConnector, HDMIConnector):
     def connect_to_dvd(self, dvd_player):
         self.connect_to_device_via_rca_cable(dvd_player)
 
@@ -16,7 +46,7 @@ class Television(EntertainmentDevice):
         self.connect_device_to_power_outlet(self)
 
 
-class dvd_player(EntertainmentDevice):
+class DvdPlayer(PowerConnector, HDMIConnector):
     def connect_to_tv(self, television):
         self.connect_to_device_via_hdmi_cable(television)
 
@@ -24,7 +54,7 @@ class dvd_player(EntertainmentDevice):
         self.connect_device_to_power_outlet(self)
 
 
-class GameConsole(EntertainmentDevice):
+class GameConsole(PowerConnector, HDMIConnector, EthernetConnector):
     def connect_to_tv(self, television):
         self.connect_to_device_via_hdmi_cable(television)
 
@@ -35,7 +65,7 @@ class GameConsole(EntertainmentDevice):
         self.connect_device_to_power_outlet(self)
 
 
-class Router(EntertainmentDevice):
+class Router(PowerConnector, EthernetConnector):
     def connect_to_tv(self, television):
         self.connect_to_device_via_ethernet_cable(television)
 
